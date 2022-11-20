00fd226e275f84775fe33fe32993b1e14294e0e8
bipinbohara <bipin.bohara20@gmail.com>
2022-11-20 19:42:49 +0545

    gradle version stable

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Dipendra\OneDrive\Desktop\Chat App\flash-chat-flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Dipendra\OneDrive\Desktop\Chat App\flutter-chat"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
